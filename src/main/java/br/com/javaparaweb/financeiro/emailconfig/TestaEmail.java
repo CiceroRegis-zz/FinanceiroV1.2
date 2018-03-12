eba40b92112887a85ce10cf994b4130f1365e8da
cicinho <ciceroregis25@gmail.com>
2018-03-12 12:37:40 -0300

    commit

---
+++ src/main/java/br/com/javaparaweb/financeiro/emailconfig/TestaEmail.java
@@ -0,0 +1,10 @@
+package br.com.javaparaweb.financeiro.emailconfig;
+
+public class TestaEmail {
+
+	public static void main(String[] args) {
+		Email email = new Email();
+		email.enviaEmail();
+	}
+
+}
